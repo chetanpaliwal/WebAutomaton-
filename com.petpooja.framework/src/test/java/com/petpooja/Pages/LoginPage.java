0f402a1d15ef5b576ea7de9ce889c563fdc5cfd8
Chetan <Chetan@DESKTOP-8I843C0>
2019-03-18 12:43:39 +0530

    cp

--- com.petpooja.framework/src/test/java/com/petpooja/Pages/LoginPage.java
+++ com.petpooja.framework/src/test/java/com/petpooja/Pages/LoginPage.java
@@ -1,7 +1,10 @@
 package com.petpooja.Pages;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 
 /*
@@ -20,6 +23,9 @@ public class LoginPage {
 	@FindBy(id="email")WebElement uname;
 	@FindBy(id="password")WebElement pass;
 	@FindBy(xpath="//button[@type='submit']")WebElement loginbutton;
+	@FindBy(xpath="//*[@id=\"dropdownMenu2\"]/div")WebElement allRestaurantbtn;
+	@FindBy(xpath="//div[@class='navbar-header']//li[3]//a[1]")WebElement MyRetaurant;
+	
 	
 	public void loginToPetpooja(String usernameApplication, String passwordApplication) {
 		
@@ -27,6 +33,21 @@ public class LoginPage {
 		uname.sendKeys(usernameApplication);
 		pass.sendKeys(passwordApplication);
 		loginbutton.click();
+		
+	}
+	
+	public void SelectRes() {
+		Actions action = new Actions(driver);
+		action.moveToElement(allRestaurantbtn);
+		allRestaurantbtn.click();
+
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			System.out.println("After 2 seconds try, Not able to find OR click on your restaurant" +e.getMessage());
+		}
+		MyRetaurant.click();
 	}
 	
 }
--- com.petpooja.framework/src/test/java/com/petpooja/Testcases/LoginTestPetpooja.java
+++ com.petpooja.framework/src/test/java/com/petpooja/Testcases/LoginTestPetpooja.java
@@ -1,30 +1,45 @@
 package com.petpooja.Testcases;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
-
 import com.petpooja.Pages.BaseClass;
 import com.petpooja.Pages.LoginPage;
+
 
 /*
  * Author QA_Chetan
  */
 
-public class LoginTestPetpooja extends BaseClass{
-	
-	@Test
+public class LoginTestPetpooja extends BaseClass {
+
+	@Test(priority = 1)
 	public void loginApp() {
-		
-		logger=report.createTest("Login to Petpooja");
-		
-		LoginPage loginPage=PageFactory.initElements(driver, LoginPage.class);
-		
+
+		logger = report.createTest("Login to Petpooja");
+
+		LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
+
 		logger.info("Starting Application");
-		
+
 		loginPage.loginToPetpooja(excel.getStringData("Login", 0, 0), excel.getStringData("Login", 0, 1));
-		
+
 		logger.pass("Login Done Successfully");
+
 	}
 
-}
+	
+	@Test(priority = 2)
+	public void SelectRestaurant() throws InterruptedException {
 
+		LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
+		
+		loginPage.SelectRes();
+		
+	 }
+	 
+
+}
